9834bba0e3817ee60aeea249f75f333b5fd9aedb
dan523 <danwright523@gmail.com>
2016-08-03 17:01:21 -0400

    Adding server logic

--- src/server.js
+++ src/server.js
@@ -1,6 +1,19 @@
 /// <reference path="../typings/index.d.ts" />
 import Server from 'socket.io';
 
-export default function startServer() {
+/**
+ * Start the socket.io server
+ * 
+ * @export
+ * @param {Store} store
+ */
+export default function startServer(store) {
     const io = new Server().attach(8090);
+
+    store.subscribe(() => io.emit('state', store.getState().toJS()));
+
+    io.on('connection', (socket) => {
+        socket.emit('state', store.getState().toJS());
+        socket.on('action', store.dispatch().bind(store));
+    });
 }
