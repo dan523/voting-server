63071e1fa0bb9bf5946e2c937f312a52951623ee
dan523 <danwright523@gmail.com>
2016-08-03 15:33:17 -0400

    Added unimplemented reducer and spec

---
+++ src/reducer.js
@@ -0,0 +1,12 @@
+/// <reference path="../typings/index.d.ts" />
+
+/**
+ * Perform the action
+ * 
+ * @export
+ * @param {any} state
+ * @param {any} action
+ */
+export default function reducer(state, action) {
+    // Figure out which function to call and call it
+}
