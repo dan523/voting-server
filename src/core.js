750f1335542a04ac2f6ee5ea0c753e47e9e2da8a
dan523 <danwright523@gmail.com>
2016-08-03 14:37:22 -0400

    Added next function and spec

--- src/core.js
+++ src/core.js
@@ -1,13 +1,28 @@
 /// <reference path="../typings/index.d.ts" />
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 /**
  * Set the entries for the state
  * 
  * @export
- * @param {Map} state
+ * @param {any} state
  * @param {List} entries
  */
 export function setEntries(state, entries) {
     return state.set('entries', List(entries));
+}
+
+/**
+ * Return the next state ready for vote
+ * 
+ * @export
+ * @param {any} state
+ * @returns
+ */
+export function next(state) {
+    let entries = state.get('entries');
+    return state.merge({
+        vote: Map({pair: entries.take(2)}),
+        entries: entries.skip(2)
+    });
 }
